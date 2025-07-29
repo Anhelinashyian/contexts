052d50d64792e26a893603a9f6afae293bb6843e
Anhelina Martyniuk <anmarty@softserveinc.com>
2025-07-29 17:09:30 +0200

    added certificate to support https

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import https from "https";
+import fs from "fs";
 
 const app = express();
 app.use(express.json());
@@ -56,15 +58,15 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
+
+  const options = {
+    key: fs.readFileSync("./localhost-key.pem"),
+    cert: fs.readFileSync("./localhost.pem"),
+  };
+
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+
+  https.createServer(options, app).listen(port, () => {
+    console.log("HTTPS server running at https://localhost:5000");
   });
 })();
